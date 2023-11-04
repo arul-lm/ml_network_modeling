e3dbd2d5ce3142f88912fef720ada4876250d99d
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-11-04 08:15:32 -0700

    send

--- lib/serialize.ml
+++ lib/serialize.ml
@@ -8,7 +8,15 @@ type link_data =
   }
 [@@deriving yojson]
 
-type list_of_link_data = link_data list [@@deriving yojson]
+type vertex_data = {
+  name: string;
+  index: int
+}[@@deriving yojson]
+
+type graph = {
+  vertices: vertex_data list;
+  links: link_data list
+} [@@deriving yojson]
     
 let link_data_of_node ((module N) : (module Node_intf.Node)) =
   let conns = N.connections in
@@ -24,9 +32,23 @@ let link_data_of_node ((module N) : (module Node_intf.Node)) =
   in
   Array.iter unwind_conn conns;
   !result
+
+let vertex_data_of_node ((module N) : (module Node_intf.Node)) =
+  let devices = N.devices in
+  let (module D) = N.device in
+  let device_name = D.name in
+  let result = ref [] in
+  let open Device_intf in
+  let make_vertex {id} =
+    let name = Printf.sprintf "%s_%d" device_name id in
+    result := {name; index = id} :: !result
+  in
+  Array.iter make_vertex devices;
+  !result
   
 let serialize_links node ~file_name =
-  let link_data = link_data_of_node node in
-  let ser_links = yojson_of_list_of_link_data link_data in
-  Yojson.Safe.to_file file_name ser_links
+  let links = link_data_of_node node in
+  let vertices = vertex_data_of_node node in
+  let g = yojson_of_graph {vertices; links} in
+  Yojson.Safe.to_file file_name g
   
