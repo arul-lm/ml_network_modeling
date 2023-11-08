b67851856bd98bad1477d181addf5aca0a72f3d7
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-11-08 15:31:00 -0800

    save all

--- lib/op.ml
+++ lib/op.ml
@@ -31,6 +31,57 @@ let load_optimizer_states (module O : Optimizer) op =
   | Linear (w, b) | LayerNorm (w, b) -> Stats.(calc_opt_stats w + calc_opt_stats b)
 ;;
 
+let matmul node device dtype (m, _k, n) =
+  Tensor.make ~node ~device ~dtype [ m; n ] |> Option.get
+;;
+
+let matmul_prep w x =
+  let node = Tensor.node x in
+  let device = Tensor.device x in
+  let dtype = Tensor.dtype x in
+  assert (Tensor.node w = Tensor.node x);
+  assert (Tensor.device w = Tensor.device x);
+  assert (Tensor.dtype w = Tensor.dtype x);
+  let kx = Base.List.last_exn (Tensor.shape x) in
+  let kw = Base.List.last_exn (Tensor.shape w) in
+  let m = Tensor.numel x / kx in
+  let n = Tensor.numel w / kw in
+  assert (kx = kw);
+  let out = matmul node device dtype (m, kx, n) in
+  out, tensor_stats out
+;;
+
+let w_forward op x =
+  match op with
+  | Create t -> t, Stats.empty (Tensor.node t) (Tensor.device t)
+  | Linear (w, _b) -> matmul_prep w x
+  | LayerNorm (_w, _b) -> x, tensor_stats x
+;;
+
+let no_param_forward op x =
+  let node = Tensor.node x in
+  let device = Tensor.device x in
+  let dtype = Tensor.dtype x in
+  match op with
+  | QK (_, _) ->
+    (* Tranpose K *)
+    let shape = Tensor.shape x in
+    assert (List.length shape = 3);
+    let b = List.hd shape in
+    let mk = List.tl shape in
+    let km = List.rev mk in
+    let k_t = Tensor.make ~node ~device ~dtype (b :: km) |> Option.get in
+    let q_t = x in
+    matmul_prep q_t k_t
+  | Softmax (_, _) -> x, tensor_stats x
+;;
+
+let forward op x =
+  match op with
+  | WeightOp op -> w_forward op x
+  | NoParamOp op -> no_param_forward op x
+;;
+
 let is_weight_op = function
   | WeightOp o -> Some o
   | _ -> None
--- lib/op_intf.ml
+++ lib/op_intf.ml
@@ -21,6 +21,7 @@ module type Op = sig
   val is_weight_op : t -> weight_op option
   val load_weight : weight_op -> Stats.t
   val load_optimizer_states : (module Optimizer) -> weight_op -> Stats.t
+  val forward : t -> Tensor.t -> Tensor.t * Stats.t
   val ( @ ) : weight_op -> t
   val ( & ) : no_param_op -> t
 end
