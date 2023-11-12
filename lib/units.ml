97d8f559da115af98ebea1e81ce2ef121f8d8aac
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-11-12 12:01:04 -0800

    Basic client/server

--- lib/units.ml
+++ lib/units.ml
@@ -2,6 +2,7 @@ let kilo_b = 1024
 let mega_b = kilo_b * kilo_b
 let giga_b = kilo_b * mega_b
 let tera_b = kilo_b * giga_b
+let peta_b = kilo_b * tera_b
 let milli = Float.pow 10. (-3.)
 let micro = milli *. milli
 let nano = milli *. micro
