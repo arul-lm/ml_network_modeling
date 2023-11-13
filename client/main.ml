afa34490318d876bc0af5e089ceee4e78bf63fcb
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-11-12 20:01:08 -0800

    Graph

--- client/main.ml
+++ client/main.ml
@@ -36,25 +36,32 @@ module Model = struct
 end
 
 module Topo = struct
-  type t = Nvidia_Fat_Tree of unit [@@deriving sexp_of, typed_variants]
+  type t = Nvidia_DGX_Fat_Tree of unit [@@deriving sexp_of, typed_variants]
 
   let label_for_variant = `Inferred
-  let initial_choice = `First_constructor
+  let initial_choice = `Empty
 
   let form_for_variant : type a. a Typed_variant.t -> a Form.t Computation.t = function
-    | Nvidia_Fat_Tree -> Bonsai.const (Form.return ())
+    | Nvidia_DGX_Fat_Tree -> Bonsai.const (Form.return ())
   ;;
 end
 
 let topo_form = Form.Typed.Variant.make (module Topo)
 let model_form = Form.Typed.Variant.make (module Model)
+let handle_click _e = Effect.print_s (Sexplib0.Sexp.Atom "loading graph...")
 
 let component =
   let%map.Computation dyn = topo_form
   and model = model_form in
+  let graph_btn =
+    Vdom.Node.button
+      ~attrs:[ Vdom.Attr.on_click handle_click ]
+      [ Vdom.Node.Text "show graph" ]
+  in
   Vdom.Node.div
     [ Form.View.to_vdom (Form.view (Form.label "Select Topology" dyn))
     ; Form.View.to_vdom (Form.view (Form.label "Select Model" model))
+    ; graph_btn
     ]
 ;;
 
